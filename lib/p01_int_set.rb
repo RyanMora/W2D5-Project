a2db6cb1cd6fd63feaec85800e1d10b2386b8765
RyanMora <ryan.aimco@gmail.com>
2017-04-09 14:07:58 -0700

    finished phase one

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,107 @@
+require 'byebug'
+
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    raise "Out of bounds" if num > @store.length - 1 || num < 0
+    @store[num] = true
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+
+  end
+
+  def validate!(num)
+
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    @store[num % @store.length] << num unless @store[num % @store.length].include?(num)
+  end
+
+  def remove(num)
+    @store[num % @store.length].delete(num)
+  end
+
+  def include?(num)
+    @store[num % @store.length].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    i = num % num_buckets
+    @store[i] << num unless @store[i].include?(num)
+    @count += 1
+    resize! if @count == num_buckets
+  end
+
+  def remove(num)
+    i = num % num_buckets
+    @count -= 1
+    @store[i].delete(num)
+  end
+
+  def include?(num)
+    i = num % num_buckets
+    @store[i].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    store2 = @store.dup
+    @count = 0
+    @store = Array.new(num_buckets * 2) {Array.new}
+    store2.each do |bucket|
+      bucket.each do |el|
+        insert(el)
+      end
+    end
+  end
+end
